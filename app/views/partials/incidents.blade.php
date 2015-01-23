594b583f300b2fd0f9935e7765cf29ab833084f4
James Brooks <james@bluebaytravel.co.uk>
2015-01-23 09:49:47 +0000

    Display scheduled statuses within the timeline differently

--- app/views/partials/incidents.blade.php
+++ app/views/partials/incidents.blade.php
@@ -5,15 +5,20 @@
         <div class="moment {{ $incidentID === 0 ? 'first' : null }}">
             <div class="row event clearfix">
                 <div class="col-sm-1">
+                    @if(!$incident->isScheduled)
                     <div class="icon status-{{ $incident->status }}">
                         <i class="{{ $incident->icon }}"></i>
                     </div>
+                    @else
+                    <div class="icon status-scheduled">
+                        <i class="ion ion-calendar"></i>
+                    </div>
+                    @endif
                 </div>
                 <div class="col-xs-10 col-xs-offset-2 col-sm-11 col-sm-offset-0">
                     <div class="panel panel-message">
                         <div class="panel-heading">
-                            <strong>{{ $incident->name }}</strong>
-                            <br>
+                            <h4>{{ $incident->name }}</h4>
                             <small class="date">
                                 <abbr class="timeago" data-toggle="tooltip" data-placement="right" title="{{ $incident->created_at_formated }}" data-timeago="{{ $incident->created_at_iso }}">
                                 </abbr>
